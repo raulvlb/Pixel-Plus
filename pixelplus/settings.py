388030e6022c5bc5bb2cbbeeca774eb32c0bc9ed
raulvlb <54158727+raulvlb@users.noreply.github.com>
2022-07-16 10:06:11 -0300

    deploy

--- pixelplus/settings.py
+++ pixelplus/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!j^p!1gi-*yym-au%3#mtq$2t^+w0@fz+d)*npvs7=a-s+j(m-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
